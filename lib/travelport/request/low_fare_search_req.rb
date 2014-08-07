b7c558f73e67b0cd339eeaaa803f9de283f4ed14
Expleo <michal.kramer@expleosoftware.com>
2014-08-07 10:57:05 +0100

    common to 17
    Signed-off-by: Expleo <michal.kramer@expleosoftware.com>

--- lib/travelport/request/low_fare_search_req.rb
+++ lib/travelport/request/low_fare_search_req.rb
@@ -14,7 +14,7 @@ module Travelport::Request
     default_for :provider_code, '1G'
     default_for :cabin, 'Economy'
     default_for :xmlns, 'http://www.travelport.com/schema/air_v20_0'
-    default_for :xmlns_common, 'http://www.travelport.com/schema/common_v24_0'
+    default_for :xmlns_common, 'http://www.travelport.com/schema/common_v17_0'
 
     validates_presence_of :sectors
     validates_presence_of :adults
