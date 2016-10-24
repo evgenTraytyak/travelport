7a94f02c12c62b3d8e489d265ffb145c93f2f754
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-10-24 14:36:25 +0300

    Refactor a little bit

--- lib/travelport/model/attribute_methods.rb
+++ lib/travelport/model/attribute_methods.rb
@@ -2,7 +2,7 @@ require 'active_support/all'
 module Travelport::Model::AttributeMethods
   def attributes
     HashWithIndifferentAccess[instance_variables.map do |var|
-      [var.to_s.gsub("@", ""), instance_variable_get(var)] unless instance_variable_get(var).nil?
+      [var.to_s.delete("@"), instance_variable_get(var)] unless instance_variable_get(var).nil?
     end.compact]
   end
 
@@ -10,13 +10,15 @@ module Travelport::Model::AttributeMethods
     new_hash = {}
 
     hash.each do |key, val|
-      updated_value = val.is_a?(Hash) ? update_keys(val) : val
+      if (val.is_a?(Hash))
+        updated_value = update_keys(val)
+      elsif (val.is_a?(Array))
+        updated_value = val.map{ |e| e.is_a?(Hash) ? update_keys(e) : e }
+      else
+        updated_value = val
+      end
 
-      updated_value = val.is_a?(Array) ?
-        val.map{ |e| e.is_a?(Hash) ? update_keys(e) : e } :
-        updated_value
-
-      updated_key = "#{key.to_s.gsub('@', '')}".to_sym
+      updated_key = key.to_s.delete('@').to_sym
 
       new_hash[updated_key] = updated_value
     end
@@ -26,12 +28,12 @@ module Travelport::Model::AttributeMethods
 
   def update_attributes(hash)
     hash.each do |key, val|
-      attribute_setter = "#{key.to_s.gsub('@', '')}=".to_sym
+      attribute_setter = "#{key.to_s.delete('@')}=".to_sym
 
       updated_value = val.is_a?(Hash) ? update_keys(val) : val
 
-      if self.respond_to?(attribute_setter)
-        self.send(attribute_setter, updated_value)
+      if respond_to?(attribute_setter)
+        send(attribute_setter, updated_value)
       else
         # unrecognised option
       end
