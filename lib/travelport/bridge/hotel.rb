bcd3096ac4d14b152dec3b8ca25b00ba75e8d7b7
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-10-26 15:08:25 +0300

    Add ability to find hotels by coordinates

--- lib/travelport/bridge/hotel.rb
+++ lib/travelport/bridge/hotel.rb
@@ -4,10 +4,9 @@ module Travelport::Bridge
     default_for :service, 'HotelService'
     default_for :document, 'hotel_v38_0/Hotel.wsdl'
 
-    def search_availability(location, options)
-      options.assert_valid_keys(:adults, :rooms, :checkin, :checkout)
+    def search_availability(options)
+      options.assert_valid_keys(:location, :coordinates, :adults, :rooms, :checkin, :checkout)
       req = Travelport::Request::HotelSearchAvailabilityReq.new do |obj|
-        obj.location = location
         options.each {|k,v| obj.send("#{k}=",v) }
       end
       Travelport::Response::HotelSearchAvailabilityRsp.new(send_request(req))
