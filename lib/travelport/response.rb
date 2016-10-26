a377a6e1834c4c0f6659ed839f7a444ed645f580
Mikhail Dyomin <mikhaildyomin@gmail.com>
2016-10-26 11:48:14 +0300

    Edit request and response rb files

--- lib/travelport/response.rb
+++ lib/travelport/response.rb
@@ -1,6 +1,7 @@
 module Travelport::Response
 end
-require "travelport/response/base"
-require "travelport/response/low_fare_search_rsp"
-require "travelport/response/hotel_search_availability_rsp"
-require "travelport/response/hotel_details_rsp"
+require 'travelport/response/base'
+require 'travelport/response/low_fare_search_rsp'
+require 'travelport/response/hotel_search_availability_rsp'
+require 'travelport/response/hotel_details_rsp'
+require 'travelport/response/hotel_create_reservation_rsp'
