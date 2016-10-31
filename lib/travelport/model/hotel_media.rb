a25028613e15e5d5b3226955d1d4c34fd55b2c55
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-10-31 18:32:57 +0300

    Add hotel media model. Bump to v0.0.11

---
+++ lib/travelport/model/hotel_media.rb
@@ -0,0 +1,4 @@
+class Travelport::Model::HotelMedia < Travelport::Model::Base
+  attr_accessor :hotel_property,
+                :media_item
+end
--- lib/travelport/response/hotel_media_links_rsp.rb
+++ lib/travelport/response/hotel_media_links_rsp.rb
@@ -1,4 +1,11 @@
 module Travelport::Response
   class HotelMediaLinksRsp < Base
+    def hotel_media_result
+      @hotel_media_result ||= if results = raw_content[:hotel_property_with_media_items]
+                                Travelport::Model::HotelMedia.new(results)
+                              else
+                                []
+      end
+    end
   end
 end
